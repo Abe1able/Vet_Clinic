9a697a1af099b96b9ac0a30aff72c3786de28b37
Abel Seifu <abel.workmail@gmail.com>
2023-02-28 23:26:02 +0300

    Create and populate animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+ id SERIAL PRIMARY KEY,
+ name TEXT,
+ date_of_birth DATE,
+ escape_attempts INT,
+ neutered BOOLEAN,
+ weight_kg NUMERIC
 );
